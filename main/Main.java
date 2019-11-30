fe5e1ca588eacd0f67b75a73293c27278fc204b1
Alex Careja <44427360+alexcareja@users.noreply.github.com>
2019-11-30 23:42:33 +0200

    Add files via upload
    Changed the reading and writing from and to files to using FileSystem

--- main/Main.java
+++ main/Main.java
@@ -1,45 +1,40 @@
 package main;
 
-import fileio.implementations.FileReader;
+import fileio.FileSystem;
 import java.io.IOException;
 import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException {
-        String file_name = args[0];
-        FileReader fileReader = new FileReader(file_name);
-        int N = fileReader.nextInt();
-        int M = fileReader.nextInt();
-        GameMap map = GameMap.getInstance(M, N, fileReader);
+        String in_name = args[0];
+        String out_name = args[1];
+        FileSystem fileSystem = new FileSystem(in_name, out_name);
+        int N = fileSystem.nextInt();
+        int M = fileSystem.nextInt();
+        GameMap map = GameMap.getInstance(M, N, fileSystem);
         ArrayList<Hero> heroes = new ArrayList<>();
-        int no_heroes = fileReader.nextInt();
+        int no_heroes = fileSystem.nextInt();
         Hero hero;
         int x, y;
         HeroesFactory heroesFactory = HeroesFactory.getInstance();
         for(int i = 0; i < no_heroes; i++) {
-            String type = fileReader.nextWord();
+            String type = fileSystem.nextWord();
             hero = heroesFactory.getHeroByChar(type.charAt(0));
             heroes.add(hero);
-            x = fileReader.nextInt();
-            y = fileReader.nextInt();
+            x = fileSystem.nextInt();
+            y = fileSystem.nextInt();
             map.placeHero(hero, x, y);
         }
-        int no_rounds = fileReader.nextInt();
+        int no_rounds = fileSystem.nextInt();
         String moves;
         for(int i = 0; i < no_rounds; i++){
-            /*TODO
-            -take dmg from previous rounds
-            -read hero movement
-            -move if not immobilized or dead
-            -fight
-             */
             for(Hero h:heroes) {
                 if(!h.isDead()){
                     h.applyDebuff();
                     h.checkHp();
                 }
             }
-            moves = fileReader.nextWord();
+            moves = fileSystem.nextWord();
             int j = 0;
             char move;
             for(Hero h:heroes) {
@@ -55,16 +50,18 @@ public class Main {
                 h.checkHp();
                 h.levelUp();
             }
-
         }
         for(Hero h:heroes) {
-            h.printHeroClass();
+            fileSystem.writeWord(h.getHeroClass());
             if(h.getHp() > 0){
-                h.printStatus();
-                map.printPos(h);
+                fileSystem.writeWord(h.getStatus());
+                fileSystem.writeWord(map.getPos(h));
+                fileSystem.writeNewLine();
                 continue;
             }
-            System.out.println("dead");
+            fileSystem.writeWord("dead");
+            fileSystem.writeNewLine();
         }
+        fileSystem.close();
     }
 }
