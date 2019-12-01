1c173078254a51324fdd1747290334394e49842a
Alex Careja <44427360+alexcareja@users.noreply.github.com>
2019-12-01 14:00:00 +0200

    Delete Visitable.java
    refactoring

--- main/Visitable.java
+++
@@ -1,9 +0,0 @@
-package main;
-
-interface Visitable {
-    /**
-     * @param v - eroul care provoaca daune
-     * @param land - taramul pe care se desfasoara lupta
-     */
-    void takeDmg(Visitor v, LandType land);
-}
